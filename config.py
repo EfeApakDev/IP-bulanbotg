ed71c6796a7950e18ed752763183aa2270f3d35a
EfeApakDev <93475651+EfeApakDev@users.noreply.github.com>
2024-07-28 17:38:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import path, getenv
 
 class config:
-    API_ID = int(getenv("API_ID", "29365506"))
-    API_HASH = getenv("API_HASH", "0bacb8797f8ddc47529e41682b40d797")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7294309547:AAGsWrbjHK3MY3wUADGgsChD59qkKDwxwxw")
+    API_ID = int(getenv("29365506", "29365506"))
+    API_HASH = getenv("0bacb8797f8ddc47529e41682b40d797", "0bacb8797f8ddc47529e41682b40d797")
+    BOT_TOKEN = getenv("7294309547:AAGsWrbjHK3MY3wUADGgsChD59qkKDwxwxw", "7294309547:AAGsWrbjHK3MY3wUADGgsChD59qkKDwxwxw")
     IP_API =getenv("ACCESS_TOKEN","IP-API API KEY IS HERE")
     
 con = config()
